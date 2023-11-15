ef6735ba06f1620905ae0f71fc4e6d8bb7fa826d
dendi <dendiasep@gmail.com>
2023-11-15 12:40:24 +0700

    Depelop the layouts of TaskList and TaskProgress pages

--- src/features/tasks/TaskAtoms.ts
+++ src/features/tasks/TaskAtoms.ts
@@ -1,8 +1,9 @@
 import { atom } from 'recoil'
+import {AtomKeys} from '../../constants/recoilKeys' // 
 import type { Task } from '../../types'
 
 export const tasksState = atom<Task[]>({
-  key: 'tasks',
+  key: AtomKeys.TASKS,
   default: [
     {
       id: 1,
