ccf6d2d0ac10ca8b78e49f71445955dbe8edc6a8
Asep Dendi <142903437+dendiasep@users.noreply.github.com>
2023-12-17 10:22:50 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import SideMenuLayout from './layouts/SideMenuLayout'
 import { RecoilRoot } from 'recoil'
