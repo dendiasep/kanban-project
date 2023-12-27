c00ccc0ffb6184dee54a88978aba97a29c3b6906
Asep Dendi <142903437+dendiasep@users.noreply.github.com>
2023-12-27 09:38:57 +0700

    Update SideMenu.tsx

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from 'react-router-dom'
-import type { CSSProperties } from '../types';
+import type { CSSProperties } from '../types/index';
 
 const SideMenu = (): JSX.Element => {
     return (
